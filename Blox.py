b075774eac9870e4177df618cefc540ec28e5fbf
Teckguru <144040040+Teckguru@users.noreply.github.com>
2023-09-04 10:18:56 -0500

    Update Blox.py

--- Blox.py
+++ Blox.py
@@ -1,41 +1,43 @@
+no = 0 
+kilo = 7500 
+spin = 10000 
+chop = 100000 
+spring = 20000 
+bomb = 40000 
+smoke = 200000 
+spike = 100000 
+flame = 450000 
+falcon = 200000 
+ice = 800000 
+sand = 750000 
+dark = 1000000 
+revive = 450000 
+diamond = 550000 
+light = 900000 
+love = 1750000 
+rubber = 400000 
+barrier = 400000 
+magma = 1100000 
+quake = 1100000 
+buddha = 2400000 
+spider = 1800000 
+phoenix = 1700000 
+portal = 2200000 
+rumble = 2300000 
+paw = 1750000 
+blizzard = 2600000
+blizard = 2600000
+gravity = 2300000
+dough = 4900000
+shadow = 3300000
+venom = 4000000
+control = 2600000
+spirit = 3400000
+dragon = 4100000
+leopard = 7800000
+lepard = 7800000  
+leo = 7800000
 
-     blank  = 0,  none  = 0,  0  = 0,  nothing  = 0,  no  = 0,
-     kilo  = 7500,
-     spin  = 10000,
-     chop  = 100000,
-     spring  = 20000,
-     bomb  = 40000,
-     smoke  = 200000,
-     spike  = 100000,
-     flame  = 450000,
-     falcon  = 200000,  bird : falcon  = 200000,
-     ice  = 800000,
-     sand  = 750000,
-     dark  = 1000000,
-     revive  = 450000,
-     diamond  = 550000,  daimond  = 550000,  dimond  = 550000,
-     light  = 900000,
-     love  = 1750000,
-     rubber  = 400000,
-     barrier  = 400000,
-     magma  = 1100000,
-     quake  = 1100000,
-     buddha  = 2400000,  human : buddha  = 2400000,  buddah  = 2400000,  budha  = 2400000,
-     string  = 1800000,  spider  = 1800000,
-     phoenix  = 1700000,  bird : pheonix  = 1700000,  pheonix  = 1700000,  phenix  = 1700000,  fenix  = 1700000,
-     portal  = 2200000,
-     rumble  = 2300000,
-     paw  = 1750000,
-     blizzard  = 2600000,  blizard  = 2600000,
-     gravity  = 2300000,
-     dough  = 4900000,
-     shadow  = 3300000,
-     venom  = 4000000,
-     control  = 2600000,
-     spirit  = 3400000,
-     dragon  = 4100000,
-     leopard  = 7800000,  lepard  = 7800000,  leo  = 7800000
-}
 
 offerfruit1 = input("Enter the first fruit you are offering: ").lower()
 offerfruit2 = input("Enter the second fruit you are offering: ").lower()
